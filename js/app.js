15c63420f220bd7f4e82ff5febd34e66d46de272
MrCooper42 <mr.cooper42co@gmail.com>
2016-08-21 20:07:38 -0600

    finished but could do opener and closer

--- js/app.js
+++ js/app.js
@@ -1,74 +1,8 @@
 $(document).ready(function() {
     var input = $('.buttons span');
-    // console.log(input);
     var equals = $('#equals');
     var output = $('#screen');
     var operator = $(".operator");
-    console.log(output);
-
-    // /set if function with regex to check if .text() has /[0-9]/ then add to below function, create set area for the first set of numbers, and THEN think about adding a period funciton.
-
-    $(input).click(function() {
-        if ($(this).hasClass('operator') !== true) {
-            console.log("this text b4", $(this).text());
-            if ($(output).data("fromPrevious") == true) {
-                resetCalc($(this).text());
-                console.log("first if this text", $(this).text());
-            } else if (($(output).data("isPendingFunc") == true) && ($(output).data("firstValSet") == false)) {
-
-                $(output).data("firstVal", $(output).val());
-                $(output).data("firstValSet", true);
-
-                $(output).val($(this).text());
-                $(output).data("secondVal", $(output).val());
-                $(output).data("secondValSet", true);
-                console.log("else if 1 this", this);
-                // clicking a num again, after first num is set and ready for second num
-            } else if (($(output).data("isPendingFunc") == true) && ($(output).data("firstValSet") == true)) {
-
-                var curValue = $(output).val();
-                var toAdd = $(this).text();
-                console.log("else if2 toadd", toAdd);
-                var newVal = curValue + toAdd;
-
-                $(output).val(newVal);
-
-                $(output).data("secondVal", $(output).val());
-                $(output).data("secondValSet", true);
-                //clicking on number When screen is clear
-            } else {
-                var curValue = $("#screen").val();
-                if (curValue == "0") {
-                    curValue = "";
-                }
-                console.log("else output", output);
-
-                let toAdd = $(this).text();
-                var newVal = curValue + toAdd;
-                $(output).val(newVal)
-                console.log("output", output);
-                console.log("newval", newVal);
-
-            }
-        }
-    });
-
-    $(operator).click(function() {
-        // var curValue = $("#screen").val();
-        // $(output).val(curValue + $(this).text())
-        if ($(output).data("fromPrevious") == true) {
-            resetCalc($(output).val());
-            $(output).data("firstValSet", false);
-            $(output).data("fromPrevious", false)
-        }
-        var pendingFunc = $(this).text();
-        $(output).data("isPendingFunc", true);
-        $(output).data("thePendingFunc", pendingFunc);
-        console.log(pendingFunc);
-        $(operator).removeClass("pendingFunc");
-        $(this).addClass("pendingFunc");
-
-    })
 
     function resetCalc(curValue) {
         $(output).val(curValue);
@@ -81,67 +15,114 @@ $(document).ready(function() {
         $(output).data('secondVal', 0);
         $(output).data('fromPrevious', false);
     }
+    resetCalc(0)
 
+    $(input).click(function() {
+        if ($(this).hasClass('operator') !== true) {
+
+            if ($(output).data("fromPrevious") == true) {
+                resetCalc($(this).text());
+
+            } else if (($(output).data("isPendingFunc") == true) && ($(output).data("firstValSet") == false)) {
+
+                $(output).data("firstVal", $(output).val());
+                $(output).data("firstValSet", true);
+
+                $(output).val($(this).text());
+                $(output).data("secondVal", $(output).val());
+                $(output).data("secondValSet", true);
+                console.log($(output).data("secondVal"));
+                // clicking a num again, after first num is set and ready for second num
+            } else if (($(output).data("isPendingFunc") == true) && ($(output).data("firstValSet") == true)) {
+
+                var curValue = $(output).val();
+                var toAdd = $(this).text();
+                console.log("else if2 output data", $(output).data());
+                console.log($(output).data("secondVal"));
+                console.log($(output).data("firstVal"));
+                var newVal = curValue + toAdd;
+
+                $(output).val(newVal);
+
+                $(output).data("secondVal", $(output).val());
+                $(output).data("secondValSet", true);
+                //clicking on number When screen is clear
+            } else {
+                let curValue = $("#screen").val();
+                if (curValue == "0") {
+                    curValue = "";
+                }
+
+                let toAdd = $(this).text();
+                var newVal = curValue + toAdd;
+                $(output).val(newVal)
+            }
+        }
+    });
+
+    // This sets operator (ex C and =) and sets next number to clear screen
+    $(operator).click(function() {
+        if (($(this).text() != "=") && ($(this).text() != "C")) {
+            console.log($(this).text());
+            if ($(output).data("fromPrevious") == true) {
+                resetCalc($(output).val());
+                $(output).data("firstValSet", false);
+                $(output).data("fromPrevious", false)
+            }
+
+            var pendingFunc = $(this).text();
+            $(output).data("isPendingFunc", true);
+            $(output).data("thePendingFunc", pendingFunc);
+
+            $(operator).removeClass("pendingFunc");
+            $(this).addClass("pendingFunc");
+        } else {
+
+        }
+    })
+
+    //this does equation without eval()
     $("#equals").click(function() {
         if (($(output).data("firstValSet") == true) && ($(output).data("secondValSet") == true)) {
-            if ($(output).data(thePendingFunc) == "+") {
+            if ($(output).data("thePendingFunc") == "+") {
                 var answer = parseFloat($(output).data("firstVal")) + parseFloat($(output).data("secondVal"))
-            } else if ($(output).data(thePendingFunc) == "-") {
+            } else if ($(output).data("thePendingFunc") == "-") {
                 var answer = parseFloat($(output).data("firstVal")) - parseFloat($(output).data("secondVal"))
-            } else if ($(output).data(thePendingFunc) == "*") {
+            } else if ($(output).data("thePendingFunc") == "x") {
                 var answer = parseFloat($(output).data("firstVal")) * parseFloat($(output).data("secondVal"))
-            } else if ($(output).data(thePendingFunc) == "/") {
+            } else if ($(output).data("thePendingFunc") == "÷") {
                 var answer = parseFloat($(output).data("firstVal")) / parseFloat($(output).data("secondVal"))
             }
 
             $(output).val(answer);
 
-            resetCalc(finalValue);
+            resetCalc(answer);
             $(output).data("fromPrevious", true);
         } else {
-
+            $(output).val(42 + " ERROR");
         }
     });
 
+    $("#clear").click(function() {
+        resetCalc(0)
+    })
+
+    $("#calculator").draggable();
+
+    // $("#opener, #closer ").click(function() {
+    //     $("#opener").toggle();
+    //     $("#calculator").toggle();
+    // }); //would be cool but didnt feel like writing the extra code
+
+
+    // Bonus 3 with eval
+    $(output).keyup(function(e) {
+        if (e.keyCode == 13) { // enter
+            $(output).val(eval($(output).val()))
+        }
+        if (e.keyCode == 27) { // esc
+            resetCalc('')
+        }
+    });
 
 });
-
-//
-//         $(output).append($(this).html());
-//         console.log("nin", input);
-//         // newOut = $(this).text();
-//         // console.log("new", newOut);
-//         //$(this).appendTo
-//         // console.log(input.text());
-//         console.log('output', output);
-//     }
-// });
-//
-// //
-// $('.operator').click(function() {
-//         var operator = $(this).text();
-//         if (operator.innerText() === '-' && output.val().length === 0) {
-//             output.val('-');
-//         } else {
-//
-//         }
-//     })
-//
-//    function //  do your clear work here
-//
-
-// operators function
-//
-// }
-//
-// equals function
-//
-// }
-//
-//funcition sor setting str to num
-//
-// }
-//
-// function for creating first held numbers
-// set to match opperator to new numbers after
-// try more es6 and regex
